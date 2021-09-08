a32cd718965790856a9488ebe481da526c649b4c
Glauber Luciano <glauber.luciano@hotmail.com>
2021-09-08 16:20:35 -0300

    Criado atributos de instade de instancia nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,8 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá Glauber  {id(self)}'
 
@@ -7,3 +11,8 @@ if __name__ == '__main__':
     print(Pessoa.cumprimentar(p))
     print(p.cumprimentar())
     print(id(p))
+    print(p.nome)
+    p.nome = 'Glauber'
+    print(p.nome)
+    print((p.idade))
+
